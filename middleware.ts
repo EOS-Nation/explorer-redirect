52e99763969e5efd037bdeb2f8f33b64ea4cf50d
Denis Carriere <carriere.denis@gmail.com>
2022-07-24 12:45:21 -0400

    update to regex

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,14 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
-  const [ part1, part2 ] = request.nextUrl.pathname.split("transaction/");
-  if ( part2 ) {
-    const [ trx_id ] = part2.split("/");
-    const url = `https://eosauthority.com/transaction/${trx_id}?network=eos`
+  const match = request.nextUrl.pathname.match(/transaction\/([\w]*)/);
+  if ( match?.length ) {
+    const url = `https://eosauthority.com/transaction/${match[1]}?network=eos`
     return NextResponse.redirect(new URL(url, request.url));
   }
-  return NextResponse.redirect(new URL('https://eosauthority.com/', request.url));
+  // return NextResponse.redirect(new URL('https://eosauthority.com/', request.url));
+}
+
+export const config = {
+  runtime: 'experimental-edge',
 }
