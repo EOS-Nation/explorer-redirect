d8fc70237faa8299eda1ea0fbe1f503bf3001303
Denis Carriere <carriere.denis@gmail.com>
2022-07-24 12:53:25 -0400

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,17 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
+  // Homepage redirect
+  if ( request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL('https://eosauthority.com/', request.url));
+  }
+
+  // Transaction ID redirection
   const match = request.nextUrl.pathname.match(/transaction\/([\w]*)/);
   if ( match?.length ) {
     const url = `https://eosauthority.com/transaction/${match[1]}?network=eos`
     return NextResponse.redirect(new URL(url, request.url));
   }
-  // return NextResponse.redirect(new URL('https://eosauthority.com/', request.url));
 }
 
 export const config = {
