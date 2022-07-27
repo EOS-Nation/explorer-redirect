a5013d91f5826b5a82c515b5baac296fe6f9bbee
Denis Carriere <carriere.denis@gmail.com>
2022-07-27 14:54:15 -0400

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,22 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from "next/server"
 
+const PROVIDER = process.env.PROVIDER || "bloks";
+
 export function middleware(request: NextRequest) {
-  // Homepage redirect
-  if ( request.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL('https://eosauthority.com/', request.url));
-  }
+  let url = "";
+  if ( PROVIDER == "bloks") url = 'https://bloks.io/';
+  if ( PROVIDER == "authority") url = 'https://eosauthority.com/';
 
   // Transaction ID redirection
+  // ex: https://explorer.eosnetwork.com/transaction/d8e5ebcb2eff590acf75b972253993c1b3b7c3ad9792f37a01ba920e47764b8e
   const match = request.nextUrl.pathname.match(/transaction\/([\w]*)/);
   if ( match?.length ) {
-    const url = `https://eosauthority.com/transaction/${match[1]}?network=eos`
-    return NextResponse.redirect(new URL(url, request.url));
+    url += `transaction/${match[1]}`;
+    if ( PROVIDER == "authority") url += '?network=eos';
   }
+
+  return NextResponse.redirect(new URL(url, request.url));
 }
 
 export const config = {
